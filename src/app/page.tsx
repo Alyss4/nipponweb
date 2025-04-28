415527308d7fb55f03dd56c02d94b31cfe2f98cf
Alyss4 <alyssafc8@gmail.com>
2025-04-28 23:02:21 +0200

    fix: icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
 'use client'
 import Button, { ButtonStyles } from '../components/componentsPages/AccueilButton';
 import { 
-  FaUser, FaTrophy, FaBookOpen, FaClipboardList, 
-  FaUsersCog, FaCheckCircle, FaUserCircle, FaTags 
+  FaUserPlus, FaTrophy, FaBookOpen, FaClipboardList, 
+  FaUsersCog, FaCheckCircle, FaUserCircle, FaTags, 
+  FaMedal, FaLayerGroup
 } from 'react-icons/fa';
+
 
 export default function Dashboard() {
   return (
     <>
       <ButtonStyles />
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '24px', justifyContent: 'center' }}>
-        <Button icon={FaUser} label="Ajouter des participants" href="/" />
-        <Button icon={FaTrophy} label="Créer / Commencer un tournoi" href="/" />
-        <Button icon={FaBookOpen} label="Guide utilisateur" href="/" />
-        <Button icon={FaUserCircle} label="Mon profil" href="/" />
-        <Button icon={FaClipboardList} label="Voir mes tournois" href="/" />
-        <Button icon={FaClipboardList} label="Consulter mes résultats" href="/" />
-        <Button icon={FaUsersCog} label="Gérer les utilisateurs" href="/" />
-        <Button icon={FaCheckCircle} label="Valider catégories / tournois" href="/" />
-        <Button icon={FaTags} label="Créer des catégories" href="/" />
+        <Button icon={FaLayerGroup} label="Créer des catégories" href="/categorie" />
+        <Button icon={FaTrophy} label="Créer un tournois" href="./tournois" />
+        <Button icon={FaUserPlus} label="Ajouter des participants" href="./gestionparticipants" />
+        <Button icon={FaUserCircle} label="Mon profil" href="/profil" />
+        <Button icon={FaUsersCog} label="Gérer les utilisateurs" href="/roles" />
+        {/* A faire a la fin <Button icon={FaBookOpen} label="Guide utilisateur" href="/" /> 
+          <Button icon={FaClipboardList} label="Voir les tournois" href="/" />
+          <Button icon={FaMedal} label="Consulter les résultats" href="./resultats" />
+          <Button icon={FaCheckCircle} label="Valider catégories / tournois" href="/" /> */}
+
       </div>
     </>
   );
