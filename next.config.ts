a5560f6a4daf409d8d14a7e1a6fbe14fed00d961
Alyss4 <alyssafc8@gmail.com>
2025-07-15 20:09:46 +0200

    final

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // C'EST CETTE LIGNE QUI EST IMPORTANTE
-  // Ajoute d'autres options si besoin
+
 };
 
 export default nextConfig;
